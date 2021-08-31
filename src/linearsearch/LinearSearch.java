ffac4bdf5992b39643ac0e69f87d5198d6e85208
Mayni <>
2021-08-31 23:00:58 +0800

    update to assignment 2021

--- src/linearsearch/LinearSearch.java
+++ src/linearsearch/LinearSearch.java
@@ -24,32 +24,126 @@ public class LinearSearch {
     public static void main(String[] args) {
         // TODO code application logic here
         arrayclass arre = new arrayclass();
-        int asksize = 0, menu = 0, numsearch = 0;
+        int asksize = 0, numsearch =0, flint = 0;
+        float flsearch=0;
+        boolean menu=false;
         Scanner input = new Scanner(System.in);
         
        // long startTime = 
-        System.out.print("Size?: ");
-        asksize = Integer.parseInt(input.nextLine());
-        if (asksize < 0) {
-            System.out.println("ayaw negative uie");
-        } else {
-            arre.set_size(asksize);
-            arre.andb();
-            arre.displayni();
-        }
+        while (flint!=3) {
+            menu=false;            
+            System.out.println("[1]Int [2]Float [3]Exit");
+            flint = Integer.parseInt(input.nextLine());
+            switch(flint){
+                case 1:
+                            
+                System.out.print("Size?: ");
+                asksize = Integer.parseInt(input.nextLine());
+                if (asksize < 0) {
+                    System.out.println("ayaw negative uie");
+                } else {
+                    arre.set_size(asksize);
+                    arre.andb();
+                    arre.displayni();
+                }
 
-        while (menu != 5) {
-            /*  menu = Integer.parseInt(JOptionPane.showInputDialog(null, "[1]Search"));
-            if (menu == 1) {*///if naay idungag
-            System.out.print("Search?: ");
-            numsearch = Integer.parseInt(input.nextLine());
-            if (numsearch > asksize - 1 || numsearch < 0) {
-                System.out.println("not on list, \nnumber too large, \nstop");
-                menu = 5;
-            } else {
-                System.out.println("index @ " + arre.search(numsearch));
+                while (menu != true) {
+                    /* menu = Integer.parseInt(JOptionPane.showInputDialog(null, "[1]Search"));
+                    if (menu == 1) {*///if naay idungag
+                    int intmenu=0;
+                    System.out.println("[1]Seach using number [2] Display Array [3]Exit");
+                    intmenu = Integer.parseInt(input.nextLine());
+                    switch(intmenu){
+                        case 1:
+                            System.out.print("Number?: ");
+                            numsearch = Integer.parseInt(input.nextLine());
+                            if (numsearch > asksize - 1 || numsearch < 0) {
+                            System.out.println("not on list ");
+                            } 
+                            else{
+                            System.out.println("index @ " + arre.search(numsearch));
+                            }                
+                            break;
+                        case 2:
+                            arre.displayni();
+
+                            break;
+                        case 3:
+                            menu =true;
+
+                            break;
+                    }
+                }
+                            break;
+                    
+                case 2:
+                            
+                    System.out.print("Size?: ");
+                    asksize = Integer.parseInt(input.nextLine());
+                    if (asksize < 0) {
+                        System.out.println("ayaw negative uie");
+                    } else {
+                        arre.set_size(asksize);
+                        arre.flandb();
+                        arre.fldisplayni();
+                    }
+                    
+                    while (menu != true) {
+                    /* menu = Integer.parseInt(JOptionPane.showInputDialog(null, "[1]Search"));
+                    if (menu == 1) {*///if naay idungag
+                    int flmenu=0;
+                    System.out.println("[1]Display float array with index in a range [2] Display Array [3]Exit");
+                    flmenu = Integer.parseInt(input.nextLine());
+                    switch(flmenu){
+                        case 1:
+                            int fra=0,lra =0, sz=arre.get_size()-1;
+                            boolean ttt=false;
+                            while(ttt!=true){
+                                System.out.println("choose from 0 - " + sz + "\nFrom what index?:");
+                                fra = Integer.parseInt(input.nextLine());
+                                if (fra<=sz) {
+                                    arre.set_frange(fra);
+                                    ttt=true;
+                                } else{
+                                    System.out.println("wrong input");
+                                }
+ 
+                            }
+                            ttt=false;
+                            while (ttt!=true) {
+                                System.out.println("choose from " + fra + " - " + sz + "\nto what index?:");
+                                lra = Integer.parseInt(input.nextLine());                                
+                                if (lra <=sz && lra >= fra) {
+                                    arre.set_lrange(lra);
+                                    ttt=true;
+                                }else {
+                                    System.out.println("wrong input");
+                                }
+                            }
+                            ttt=false;
+                            arre.flwrange();
+                            break;
+                        case 2:
+                            arre.fldisplayni();
+
+                            break;
+                        case 3:
+                            menu =true;
+                            /*  keeping this basin lngSystem.out.print("Number(with decimals): ");
+                            flsearch = Float.parseFloat(input.nextLine());
+                            int hhh = Integer.parseInt(arre.flsearch(flsearch));
+                            if (flsearch > asksize - 1 || flsearch < 0 || hhh==-1) {
+                            System.out.println("not on list");
+                            } else {
+                            System.out.println("index @ " + arre.flsearch(flsearch));
+                            }*/
+
+                            break;
+                    }
+                }
+                            break;
+
             }
-
-        }
+        }  
     }
 }
--- src/linearsearch/arrayclass.java
+++ src/linearsearch/arrayclass.java
@@ -17,8 +17,10 @@ import javax.swing.table.DefaultTableModel;
 public class arrayclass {
     //pangbilin nko mga print hh
 
-    int i = 0, j = 0, matches = 0, random = 0, size = 0;
+    int i = 0, j = 0, matches = 0, random = 0, size = 0,frange =0,lrange =0;
+    float flrandom = 0;
     int arraynisya[];
+    float flarray[];
 
     DefaultTableModel displayt = new DefaultTableModel();
     JTable table = new JTable(displayt);
@@ -26,6 +28,7 @@ public class arrayclass {
     void andb() {
         set_size(get_size());
         arraynisya = new int[size];
+        i=0;
         while (i != size) {
             int l = i;
             j = 0;
@@ -52,9 +55,46 @@ public class arrayclass {
             i++;
         }
     }
+    
+        void flandb() {
+        set_size(get_size());
+        flarray = new float[size];
+        i=0;
+        while (i != size) {
+            int l = i;
+            j = 0;
+            while (j != 1) {
+                //System.out.println("array# " + i);
+                j = 0;
+                matches = 0;
+                l = i;
+                flrandom = (float) (Math.random() * size);
+                do {
+                    flarray[i] = (float)flrandom;
+                    //System.out.println("flarray "+flarray[i]+" flrandom "+flrandom);
+                    //float array was not comparing so,,, string :'D it just works!
+                    if (Float.toString(flarray[i]) == Float.toString(flrandom)) {
+                        matches++;
+                    }
+                    //System.out.println("random =" + random + " | array " + l + "=" + arraynisya[l] + " | matches " + matches);
+                    l--;
+                } while (l >= 0);
+                if (matches > 1) {
+                    j = 0;
+                } else if (matches <= 1) {
+                    j = 1;
+                }
+            }
+            i++;
+        }
+    }
 
     void displayni() {
         System.out.println(Arrays.toString(arraynisya));
+    }
+    
+    void fldisplayni() {
+        System.out.println(Arrays.toString(flarray));
     }
 
     void set_size(int s) {
@@ -63,6 +103,22 @@ public class arrayclass {
 
     int get_size() {
         return size;
+    }
+    
+    void set_frange(int r) {
+        frange = r;
+    }
+
+    int get_frange() {
+        return frange;
+    }
+    
+    void set_lrange(int r) {
+        lrange = r;
+    }
+
+    int get_lrange() {
+        return lrange;
     }
 
     int search(int num) {
@@ -73,6 +129,39 @@ public class arrayclass {
             }
         }
         return k - 1;
+    }
+    int insearch(int num) {
+        int k = 0, i = 0;
+        for (k = 0; i != 1; k++) {
+            if (arraynisya[k] == num) {
+                i = 1;
+            }
+        }
+        return k - 1;
+    }
+    
+    String flsearch(float num) {
+        int flsz=get_size();
+        int k = 0, i = 0;
+        for (k = 0; i != 1; k++) {
+            if (flarray[k] == num ) {
+            i = 1;
+            }else if(k==flsz){
+            i=1;
+            }
+        }
+        k=k-1;
+        return Float.toString(k);
+    }
+    
+    void flwrange(){
+        int fr =0, lr =0;
+        fr = get_frange(); lr = get_lrange();
+        System.out.println("");
+        for (int k = fr; k <= lr; k++) {
+            System.out.print(flarray[k]+"@ index #"+ k+", ");
+        }
+        System.out.println("");
     }
 
 }
